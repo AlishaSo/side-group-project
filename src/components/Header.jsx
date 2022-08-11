5ccdf5ed5a44ece8eb8fe020737b499ca26f821b
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-10 21:12:09 -0400

    Add wrapper and button for mobile menu, add rest of nav links and add logic to toggle mobile menu open/closed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,32 @@
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const closeMenu = e => {
+      if(e.originalTarget.tagName != 'BUTTON' && e.originalTarget.tagName != 'SPAN')
+        setMenuOpen(false);
+    }
+    document.body.addEventListener('click', closeMenu);
+
+    return () => document.body.addEventListener('click', closeMenu);
+  }, []);
+
   return (
     <header>
-      <nav>
-        <NavLink to='/'>Home</NavLink>
-      </nav>
+      <div className='wrapper'>
+        <button className='nav-toggle'>
+          <span className='hamburguer'></span>
+        </button>
+        <NavLink to='/'>SAT 2400</NavLink>
+        <nav className={`${menuOpen ? 'visible' : ''}`}>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/get-song'>Get a song</NavLink>
+          <NavLink to='/meet-team'>The Team</NavLink>
+        </nav>
+      </div>
     </header>
   )
 }
