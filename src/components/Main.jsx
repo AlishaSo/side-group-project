d6c731b3802dab148f492549ee34ceb0f0976e59
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-10 21:29:44 -0400

    Add route for the get song component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,13 @@
 import {Routes, Route} from 'react-router-dom';
-import Homepage from './Homepage';
+import Homepage from '../pages/Homepage';
+import GetSong from '../pages/GetSong';
 
 export default function Main() {
   return (
     <main>
       <Routes>
         <Route path='/' element={<Homepage />} />
+        <Route path='/get-song' element={<GetSong />} />
       </Routes>
     </main>
   )
