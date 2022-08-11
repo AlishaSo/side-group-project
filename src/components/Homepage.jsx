c7c577d627fa4dce0bc0e7f735335498929db1b0
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-10 21:12:45 -0400

    Add text per the wireframe

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,8 @@
 export default function Homepage() {
   return (
     <div className='homepage-div'>
-
+      <h1>Welcome to, <span>_____</span>!</h1>
+      <h2>Enter your phone number to get a song specific to it, selected from the top 100 YouTube videos 🙌🏽</h2>
     </div>
   )
 }
