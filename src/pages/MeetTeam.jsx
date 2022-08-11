f98b22dbbdc20573898f233cf41171a66c006978
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-10 21:39:04 -0400

    Add skeleton code per wireframe

---
+++ src/pages/MeetTeam.jsx
@@ -0,0 +1,26 @@
+export default function MeetTeam() {
+  return (
+    <div className='meet-team-div'>
+      <h2>Meet the team 👋🏽</h2>
+
+      <div className='shane-div'>
+        <div className='emoji-div'>
+          👽
+        </div>
+        <p><span>Shane</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+      <div className='alisha-div'>
+        <div className='emoji-div'>
+          👾
+        </div>
+        <p><span>Alisha</span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+      <div className='thomas-div'>
+        <div className='emoji-div'>
+          ☕️
+        </div>
+        <p><span>Thomas</span> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+    </div>
+  )
+}
