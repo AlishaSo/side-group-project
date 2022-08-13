4f6063c2d5cb92287e64b03900b04fd45184032d
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-13 15:17:25 -0400

    Conditionally render the quote

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from 'react';
+import { getQuote } from '../services/api';
+
 export default function ErrorPage() {
+  const [quote, setQuote] = useState(null);
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      let response = await getQuote();
+      setQuote(response);
+    }
+    fetchQuote();
+  }, []);
+
   return (
     <div className='error-div'>
-      <p><q>Knowledge rests not upon truth alone, but upon error also.</q> &mdash; Carl Jung</p>
+      <p>
+        {
+          quote ? 
+            <><q>{quote.text}</q> &mdash; {quote.author}</>
+          :
+            ''
+        }
+      </p>
     </div>
   )
 }
