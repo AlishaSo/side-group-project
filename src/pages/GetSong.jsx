f1758f7896bd4e0b0cf0f289d44ab197c2616de0
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-13 19:43:19 -0400

    Remove unneeded id and fix spacing

--- src/pages/GetSong.jsx
+++ src/pages/GetSong.jsx
@@ -2,30 +2,32 @@ import { useState } from 'react';
 import DisplaySong from './DisplaySong';
 
 export default function GetSong() {
-  const [song, setSong] = useState('');
+  const [song, setSong] = useState(null);
 
   const handleSubmit = () => {
 
   }
 
   return (
-    <div id='song' className='song-div'>
-      {song ? 
-        <div className='form'>
-          <h2>Enter a phone number and get a YouTube video tailored to you</h2>
-          <form onSubmit={ handleSubmit }>
-            <input
-              placeholder='(999) 999-9999'
-            />
-            <button className='btn submit-btn'>Submit</button>
-            <br />
-            <input type='checkbox' />
-            <label>Check this box if you would like to receive the link via text*</label>
-          </form>
-          <p className='disclaimer-p'>* can only send text to US phone numbers</p>
-        </div>
+    <div className='song-div'>
+      {
+        song == null ? 
+          <div className='form'>
+            <h2>Enter a phone number and get a YouTube video tailored to you</h2>
+            <form onSubmit={ handleSubmit }>
+              <input
+                type='text'
+                placeholder='(999) 999-9999'
+              />
+              <button className='btn submit-btn'>Submit</button>
+              <br />
+              <label>
+              <input type='checkbox' />Check this box if you would like to receive the link via text*</label>
+            </form>
+            <p className='disclaimer-p'>* can only send text to US phone numbers</p>
+          </div>
         :
-        <DisplaySong />
+          <DisplaySong />
       }
     </div>
   )
