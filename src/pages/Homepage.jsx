717edebc2853f4ea3695de26a7802201a99f7e7f
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-12 23:24:15 -0400

    Change wording for app description

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,7 +2,7 @@ export default function Homepage() {
   return (
     <div className='homepage-div'>
       <h1>Welcome to, <span>_____</span>!</h1>
-      <h2>Enter your phone number to get a song specific to it, selected from the top 100 YouTube videos 🙌🏽</h2>
+      <h2>Enter your phone number to get a song specific to it, selected from the most popular YouTube videos 🙌🏽</h2>
     </div>
   )
 }
