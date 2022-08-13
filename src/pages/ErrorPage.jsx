cd2d7ef02e49ad072bb3c61b4f306c671c20b76a
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-13 13:44:21 -0400

    Create 404 page component

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,7 @@
+export default function ErrorPage() {
+  return (
+    <div className='error-div'>
+      <p><q>Knowledge rests not upon truth alone, but upon error also.</q> &mdash; Carl Jung</p>
+    </div>
+  )
+}
