ecc4d3a60e919d031dc02252b7189c22eed1aa09
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-13 13:44:55 -0400

    Add route for error page component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,14 +2,16 @@ import {Routes, Route} from 'react-router-dom';
 import Homepage from '../pages/Homepage';
 import GetSong from '../pages/GetSong';
 import MeetTeam from '../pages/MeetTeam';
+import ErrorPage from '../pages/ErrorPage';
 
 export default function Main() {
   return (
     <main>
       <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/get-song' element={<GetSong />} />
-        <Route path='/meet-team' element={<MeetTeam />} />
+        <Route path='/' element={ <Homepage /> } />
+        <Route path='/get-song' element={ <GetSong /> } />
+        <Route path='/meet-team' element={ <MeetTeam /> } />
+        <Route path='*' element={ <ErrorPage /> } />
       </Routes>
     </main>
   )
