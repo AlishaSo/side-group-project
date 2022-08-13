b388a72fa0366622e247ff9b4e10abb84706d5c6
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-13 19:37:15 -0400

    Change Shane's emoji and remove some white space

--- src/pages/MeetTeam.jsx
+++ src/pages/MeetTeam.jsx
@@ -5,22 +5,22 @@ export default function MeetTeam() {
 
       <div className='teammate-div shane-div'>
         <div className='emoji-div'>
-          <p>👽</p>
+          <p>⛄️</p>
         </div>
-        <p><span className='teammate'>Shane</span>&nbsp;&nbsp;&nbsp;Junior Software Engineer. Music Admirer. Knowledge Seeker. Record holder in 400m dash in Track & Field. “My brain is only a receiver, in the Universe there is a core from which we obtain knowledge, strength and inspiration.” 
+        <p><span className='teammate'>Shane</span>&nbsp;&nbsp;Junior Software Engineer. Music Admirer. Knowledge Seeker. Record holder in 400m dash in Track & Field. “My brain is only a receiver, in the Universe there is a core from which we obtain knowledge, strength and inspiration.” 
         &mdash; Nikola Tesla</p>
       </div>
       <div className='teammate-div alisha-div'>
         <div className='emoji-div'>
           <p>🦄</p>
         </div>
-        <p><span className='teammate'>Alisha</span>&nbsp;&nbsp;&nbsp;Frontend Developer. Music lover. Volleyball Admirer. Infinite optimist. Love a good quote, so here, have one: ✨<q>Be genuine 100%. Your complexity will be a source of strength not something you need to hide.</q>✨ &mdash; Joanna Hausmann</p>
+        <p><span className='teammate'>Alisha</span>&nbsp;&nbsp;Frontend Developer. Music lover. Volleyball Admirer. Infinite optimist. Love a good quote, so here, have one: ✨<q>Be genuine 100%. Your complexity will be a source of strength not something you need to hide.</q>✨ &mdash; Joanna Hausmann</p>
       </div>
       <div className='teammate-div thomas-div'>
         <div className='emoji-div'>
           <p>☕️</p>
         </div>
-        <p><span className='teammate'>Thomas</span>&nbsp;&nbsp;&nbsp;Creative content creator contemplating critical connundrums. Writer of intricate ideas and conscience concepts. Lover of life and serial entrepreneur. "All our knowledge begins with the senses, proceeds then to the understanding, and ends with reason. There is nothing higher than reason." &mdash; Immanuel Kant</p>
+        <p><span className='teammate'>Thomas</span>&nbsp;&nbsp;Creative content creator contemplating critical connundrums. Writer of intricate ideas and conscience concepts. Lover of life and serial entrepreneur. "All our knowledge begins with the senses, proceeds then to the understanding, and ends with reason. There is nothing higher than reason." &mdash; Immanuel Kant</p>
       </div>
     </div>
   )
