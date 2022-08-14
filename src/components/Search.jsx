5f0df2ce2eb3849d083fa764cca4a166d16fd077
jshimaru <55031303+JShimaru@users.noreply.github.com>
2022-08-14 15:18:48 -0400

    updated display component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import GOOGLE_API from '../services/Google'
 import Display from './Display';
 
 function Search(props){
     //console.log(props)
     const [videoInfo, setVideoInfo] = useState({})
-    const [videoId, setVideoId] = useState('')
+    const [videoId, setVideoId] = useState("ZLxJZpaph8M")
 
 
 let key = GOOGLE_API;
@@ -16,20 +16,28 @@ const search = async ()=>{
         let VIDEO_ID = res.data.items[props.number].id.videoId
         let videoData = res.data.items[props.number].snippet
         setVideoInfo(videoData)
+        console.log(VIDEO_ID)
         setVideoId(VIDEO_ID)
         console.log(videoId)
         console.log(videoInfo)
     })
 }
 
+    function HandleSubmit(){
+        useEffect(()=>{
+            search()
+            console.log(videoId)
+        },[])
+    }
 
-console.log(videoInfo)
+
+// console.log(videoInfo)
 
         return(
             <div>
                 {videoInfo.title !== undefined ? 
                 <><h3 className='vid title'>youtubeVideo.title</h3></> : <><br/>
-                <button className='btn submit-btn' onSubmit={()=>search()} >Submit</button>
+                <button className='btn submit-btn' onSubmit={()=>HandleSubmit()} >Submit</button>
                 <br/>
                 <input type='checkbox' />
             <label>Check this box if you would like to receive the link via text*</label></>}
